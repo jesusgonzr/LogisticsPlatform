f1d61799f02c50b00c1dd9220021725b71e25f47
Jesus Gonzalez <jesusgonzr@gmail.com>
2023-09-07 17:56:41 +0200

    Implementación de añadir pedidos a un vehiculo

--- LogisticsPlatform.Application/Command/Vehicle/AddOrderVehicleCommand.cs
+++ LogisticsPlatform.Application/Command/Vehicle/AddOrderVehicleCommand.cs
@@ -1,16 +1,11 @@
 ﻿using LogisticsPlatform.Application.Models;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LogisticsPlatform.Application.Command.Vehicle
 {
     public class AddOrderVehicleCommand : IRequest<VehicleViewModel>
     {
         public Guid Id { get; set; }
-        public Guid ProductId { get; set; }
+        public Guid OrderId { get; set; }
     }
 }
--- LogisticsPlatform.Application/Command/Vehicle/AddOrderVehicleCommandHandler.cs
+++ LogisticsPlatform.Application/Command/Vehicle/AddOrderVehicleCommandHandler.cs
@@ -1,18 +1,66 @@
-﻿using LogisticsPlatform.Application.Models;
+﻿using AutoMapper;
+using LogisticsPlatform.Application.Interfaces;
+using LogisticsPlatform.Application.Models;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace LogisticsPlatform.Application.Command.Vehicle
 {
     public class AddOrderVehicleCommandHandler : IRequestHandler<AddOrderVehicleCommand, VehicleViewModel>
     {
+        private readonly IMapper mapper;
+        private readonly IVehicleRepository repository;
+        private readonly IOrderRepositoryQueries orderRepositoryQueries;
+        private readonly IVehicleRepositoryQueries vehicleRepositoryQueries;
+        private readonly ILogger<AddOrderVehicleCommandHandler> logger;
+
+        public AddOrderVehicleCommandHandler(IMapper mapper, ILogger<AddOrderVehicleCommandHandler> logger,
+            IVehicleRepository repository, IVehicleRepositoryQueries vehicleRepositoryQueries,
+            IOrderRepositoryQueries orderRepositoryQueries)
+        {
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            this.orderRepositoryQueries = orderRepositoryQueries ?? throw new ArgumentNullException(nameof(orderRepositoryQueries));
+            this.vehicleRepositoryQueries = vehicleRepositoryQueries ?? throw new ArgumentNullException(nameof (vehicleRepositoryQueries));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         public Task<VehicleViewModel> Handle(AddOrderVehicleCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (request == null)
+                {
+                    throw new ArgumentNullException(nameof(request));
+                }
+
+                var vehicleData = this.vehicleRepositoryQueries.GetbyId(request.Id);
+                if (vehicleData == null)
+                {
+                    string menssage = $"El vehiculo {request.Id} no existe.";
+                    this.logger.LogError(menssage);
+                    throw new Exception(menssage);
+                }
+
+                var orderRepo = this.orderRepositoryQueries.GetById(request.OrderId);
+                if (orderRepo == null)
+                { 
+                    string menssage = $"El pedido {request.OrderId} no existe.";
+                    this.logger.LogError(menssage);
+                    throw new Exception(menssage);
+                }
+
+                vehicleData.AddOrder(orderRepo);
+
+                repository.Update(vehicleData);
+
+                return Task.FromResult<VehicleViewModel>(this.mapper.Map<VehicleViewModel>(vehicleData));
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "Error creating a product.", request);
+                throw;
+            }
         }
     }
 }
